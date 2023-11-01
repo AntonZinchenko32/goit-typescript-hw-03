8841693840e327ed87c489ce11af68ef0365368b
AntonZinchenko32 <zinchenko309@gmail.com>
2023-11-01 19:00:22 +0200

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -1,18 +1,39 @@
 class Key {
-    private signature:number;
-    
-    constructor() { 
-        this.signature = Math.random()
-    }
-    
-    getSignature(): number {
-        return this.signature
-    }
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
 }
 
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
 
+abstract class House {
+  protected door: boolean;
+  protected tenants: Person[] = [];
 
+  constructor(protected key: Key) {}
 
+  comeIn(tenant: Person): void {
+    if (this.door) this.tenants.push(tenant);
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  public openDoor(key: Key): void {
+    if (this.key === key) this.door = true;
+  }
+}
 
 const key = new Key();
 
@@ -22,6 +43,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
